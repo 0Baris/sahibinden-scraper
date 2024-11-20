f93975e223b41c8ac3f32567031886b7d7f7b258
Barış Cem Ant <baris.ant@ogr.gelisim.edu.tr>
2024-11-21 00:55:14 +0300

    Find items, changed “xpath” to “css selector”. Added color and year properties to the data output, added the ability to search by engine volume. The application has been optimized.

--- functions.py
+++ functions.py
@@ -1,7 +1,4 @@
 import psycopg2
-import pandas as pd
-import numpy as np
-from selenium.common import StaleElementReferenceException
 
 
 def veri_cek(ilan_model,ilan_basligi,ilan_kilometre,ilan_fiyati,ilan_tarihi,ilan_sehir):
@@ -30,58 +27,29 @@ def veri_cek(ilan_model,ilan_basligi,ilan_kilometre,ilan_fiyati,ilan_tarihi,ilan
     
     """)
 
+    ### Listelere tek tek contentleri eklem
+    try:
+        obje = {}
+        obje['İlan Başlığı'] = ilan_basligi
+        obje['Model'] = ilan_model
+        obje['Kilometre'] = ilan_kilometre
+        obje['Fiyat'] = ilan_fiyati
+        obje['Adres'] = ilan_tarihi
+        obje['Tarih'] = ilan_sehir
 
-    ### Listelere tek tek contentleri ekleme
-    for i in range(min(len(ilan_model), len(ilan_basligi), len(ilan_kilometre), len(ilan_fiyati), len(ilan_tarihi), len(ilan_sehir))):
-         try:
-            obje = {}
-            obje['İlan Başlığı'] = ilan_basligi[i].text.strip()
-            obje['Model'] = ilan_model[i].text.strip()
-            obje['Kilometre'] = ilan_kilometre[i].text.strip().replace('.', '')
-            obje['Fiyat'] = ilan_fiyati[i].text[:-3].strip().replace('.', '')
-            obje['Adres'] = ilan_tarihi[i].text.strip().replace('\n', ' ')
-            obje['Tarih'] = ilan_sehir[i].text.strip().replace('\n', ' ')
+        cursor.execute("""
+                    INSERT INTO sahibinden (baslik, model, kilometre, fiyat, adres, tarih)
+                        VALUES (%s, %s, %s, %s, %s, %s)
+                    """, (obje['İlan Başlığı'], obje['Model'], obje['Kilometre'], obje['Fiyat'], obje['Adres'], obje['Tarih']))
 
-            if all(obje.values()):
-                cursor.execute("""
-                INSERT INTO sahibinden (baslik, model, kilometre, fiyat, adres, tarih)
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                """, (obje['İlan Başlığı'], obje['Model'], obje['Kilometre'], obje['Fiyat'], obje['Adres'], obje['Tarih']))
-         except StaleElementReferenceException:
-             print("Hatalı işlem!")
-
-
+    except Exception as e:
+        print(f"Veri aktarılamadı hata kodu: {e}")
 
     connect.commit()
     cursor.close()
     connect.close()
 
-    """
-    
-    database = pd.DataFrame(items)
-
-    try:
-        ### Reklam satırlarının silinmesi
-        database = database.dropna(how="any")
-
-        ### Medyan,minimum ve maximum değerlerinin alınabilmesi için veri tipi değiştirilmesi
-        database['Kilometre'] = pd.to_numeric(database['Kilometre'].replace('', np.nan), errors='coerce')
-
-        ### Database mevcut olan 'TL' değerenin değiştirilmesi.
-        database['Fiyat'] = database['Fiyat'].str.replace('TL', '', regex=False)
-        database['Fiyat'] = database['Fiyat'].str.replace('.', '').str.strip()
-        database['Fiyat'] = pd.to_numeric(database['Fiyat'], errors='coerce')
-
-        ### Database'de mevcut olan Adresteki karakterleri temizleme.
-        database['Adres'] = database['Adres'].str.replace('\n', ' ', regex=False)
-
-        ### Database'de mevcut olan Tarihteki karakterleri temizleme.
-        database['Tarih'] = database['Tarih'].str.replace('\n', ' ', regex=False)
-
-    finally:
-    """
-
-    return print("Tamamlandı!")
+    return
 
 if __name__ == "__main__":
     print("Fonksiyonlar başarıyla içeri aktarıldı.")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from selenium.common import TimeoutException
+from selenium.common import TimeoutException, StaleElementReferenceException
 
 import functions
 import undetected_chromedriver as uc
@@ -8,6 +8,7 @@ import time
 araba = "Renault Clio"
 yıl_min = 2016
 yıl_max = 2016
+motor_hacmi = "1.5"
 
 driver = uc.Chrome()
 driver.get("https://www.sahibinden.com")
@@ -54,17 +55,40 @@ time.sleep(3)
 ### Sırasıyla her bir satırın verileri.
 while True:
     try:
-        ilan_model = driver.find_elements(by="xpath", value='//*[@id="searchResultsTable"]/tbody/tr[*]/td[3]')
-        ilan_basligi = driver.find_elements(by="xpath", value='//*[@id="searchResultsTable"]/tbody/tr[*]/td[4]')
-        ilan_kilometre = driver.find_elements(by="xpath", value='//*[@id="searchResultsTable"]/tbody/tr[*]/td[6]')
-        ilan_fiyati = driver.find_elements(by="xpath", value='//*[@id="searchResultsTable"]/tbody/tr[*]/td[8]')
-        ilan_tarihi = driver.find_elements(by="xpath", value='//*[@id="searchResultsTable"]/tbody/tr[*]/td[9]')
-        ilan_sehir = driver.find_elements(by="xpath", value='//*[@id="searchResultsTable"]/tbody/tr[*]/td[10]')
-
-        database = functions.veri_cek(ilan_model, ilan_basligi, ilan_kilometre, ilan_fiyati, ilan_tarihi, ilan_sehir)
+        ilan_model = driver.find_elements(by="css selector", value='.searchResultsTagAttributeValue')
+        ilan_basligi = driver.find_elements(by="css selector", value='.searchResultsTitleValue')
+        renk_km_yil = driver.find_elements(by="css selector", value='.searchResultsAttributeValue')
+        ilan_fiyati = driver.find_elements(by="css selector", value='.searchResultsPriceValue')
+        ilan_tarihi = driver.find_elements(by="css selector", value='.searchResultsDateValue')
+        ilan_sehir = driver.find_elements(by="css selector", value='.searchResultsLocationValue')
 
         time.sleep(2)
-        driver.find_element(by="css selector", value='.prevNextBut[title="Sonraki"]').click()
+
+        try:
+            min_len = min(len(ilan_model), len(ilan_basligi), len(ilan_fiyati), len(ilan_tarihi), len(ilan_sehir))
+
+            for i in range(min_len):
+                baslik = ilan_basligi[i].text.strip()
+                model = ilan_model[i].text.strip()
+                fiyat = ilan_fiyati[i].text[:-3].strip().replace('.', '')
+                tarih = ilan_tarihi[i].text.strip().replace('\n', ' ')
+                sehir = ilan_sehir[i].text.strip().replace('\n', ' ')
+                yil = renk_km_yil[i * 3].text.strip()
+                km = renk_km_yil[i * 3 + 1].text.strip().replace('.', '')
+                renk = renk_km_yil[i * 3 + 2].text.strip()
+
+                if motor_hacmi in model:
+
+                    database = functions.veri_cek(model, baslik, km, fiyat, tarih, sehir)
+                else:
+                    continue
+
+            driver.find_element(by="css selector", value='.prevNextBut[title="Sonraki"]').click()
+
+        except StaleElementReferenceException:
+            continue
+
+        time.sleep(1)
     except TimeoutException:
             print("Button element not found.")
 
